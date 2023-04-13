e97a1c31350028c568de84fb03672bbfcd8bab57
ramyashrict <ramyarr1415@gmail.com>
2023-04-13 11:22:16 +0530

    Added Addressbook

---
+++ PersonAdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+
+        public Person(string firstName, string lastName, string adress)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Adress = adress;
+        }
+    }
+}
+}
+
+   
+
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                bool ProgramIsRunning = true;
+                AdressBook ab = StartProgram();
+
+                Console.WriteLine("--------- AdressBook 1.0 ---------");
+
+                while (ProgramIsRunning)
+                {
+                    // Print user options
+                    PrintUserOptions();
+                    var userInput = Console.ReadLine();
+
+                    switch (userInput)
+                    {
+                        case "1":
+                            ab.CreateUser();
+                            break;
+                        case "2":
+                            ab.UpdateUserInformation();
+                            break;
+                        case "3":
+                            ab.RemovePersonFromList();
+                            break;
+                        case "4":
+                            ab.ShowAllPersonsInList();
+                            break;
+                        case "x":
+                            ProgramIsRunning = false;
+                            break;
+                    }
+                }
+            }
+
+            private static void PrintUserOptions()
+            {
+                Console.WriteLine("Choose one of the following options: ");
+                Console.WriteLine("#1 Create new user");
+                Console.WriteLine("#2 Edit user information");
+                Console.WriteLine("#3 Delete existing user");
+                Console.WriteLine("#4 Show all users in adressBook");
+            }
+
+            private static AdressBook StartProgram()
+            {
+                AdressBook ab = new AdressBook();
+
+                //Start program by loading saved users from txt-file
+                WriteAndReadToFile writer = new WriteAndReadToFile();
+                writer.ReadFromFile(ab);
+                return ab;
+            }
+        }
+
+    }
+}
+   
